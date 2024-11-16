e6417bb8458f4b3f586528e29d160cf235fd5c06
senemdemirr <senemdemir262@gmail.com>
2024-11-17 01:45:16 +0300

    completed home module

---
+++ src/app/pages/home/home.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+];
+
+
+@NgModule({
+  declarations: [HomeComponent],
+  imports: [RouterModule.forChild(routes), CommonModule],
+  exports: [HomeComponent, RouterModule],
+})
+export class HomeModule {}
